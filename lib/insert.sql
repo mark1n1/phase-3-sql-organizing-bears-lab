6801cab4364bc7d4d6883037165494fc63b764f1
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-07-24 15:35:30 -0700

    Completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 2, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 5, "M", "black", "intense", 1),
+(3,"Tabitha", 1, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 3, "M", "Green", "Slimy", 0),
+(5,"Melissa", 10, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 7, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 12, "F", "Blue", "naive", 1),
+(8,null, 4, "M", "black", "aggressive", 0);
